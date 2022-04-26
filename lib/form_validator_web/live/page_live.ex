b9393a591c3133c7a5f1c66850078d4b5dd560fb
Denis T <kiddypoker@gmail.com>
2022-04-26 22:34:43 +0300

    init

---
+++ lib/form_validator_web/live/page_live.ex
@@ -0,0 +1,33 @@
+defmodule FormValidatorWeb.PageLive do
+  use FormValidatorWeb, :live_view
+
+  alias FormValidator.Accounts
+  alias FormValidator.Accounts.User
+
+  def mount(_params, _session, socket) do
+    changeset = Accounts.change_user(%User{})
+    {:ok, assign(socket, changeset: changeset)}
+  end
+
+  def handle_event("validate", %{"user" => user_params}, socket) do
+    changeset =
+      %User{}
+      |> Accounts.change_user(user_params)
+      |> Map.put(:action, :validate)
+
+    {:noreply, assign(socket, changeset: changeset)}
+  end
+
+  def handle_event("save", %{"user" => user_params}, socket) do
+    case Accounts.create_user(user_params) do
+      {:ok, _users} ->
+        {:noreply,
+         socket
+         |> put_flash(:info, "Account created successfully")
+         |> push_redirect(to: "/")}
+
+      {:erorr, %Ecto.Changeset{} = changeset} ->
+        {:noreply, assign(socket, changeset: changeset)}
+    end
+  end
+end
